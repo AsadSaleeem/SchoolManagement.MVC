c4f4fdf1b175eef9a07dd0d2ee4033eb625b9aa8
Asad <asad_mohammad@hotmail.com>
2023-08-27 10:31:23 +0500

    Layout Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace SchoolManagement.MVC.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
